133f8663b1b3143cbea2ce991868df6f19cd50d2
Chris Kuehl <ckuehl@ckuehl.me>
2017-09-11 22:08:23 -0700

    Add package boilerplate

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from setuptools import setup
+
+
+setup(
+    name='pygments-ansi-color',
+    version='0.0.0',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=['pygments'],
+    py_modules=['pygments_ansi_color'],
+)
