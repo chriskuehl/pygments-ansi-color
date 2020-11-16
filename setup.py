6c86544c34254c03845c6c122468bb102c70e3eb
Chris Kuehl <ckuehl@yelp.com>
2020-11-16 14:34:20 -0800

    v0.0.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='pygments-ansi-color',
-    version='0.0.5',
+    version='0.0.6',
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2',
